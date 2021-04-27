985bd99cce0b14d3e701ab7ead74be1c608acb11
guillermo varela <guillovarela16@gmail.com>
2021-04-27 12:22:28 -0300

    Finish post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,50 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
+  before_action :set_post, only: %w[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %w[index show]
+
+  def index
+    @posts = Post.order(created_at: :asc)
+  end
+
+  def show; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save?
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :photo, :content)
+  end
 end
