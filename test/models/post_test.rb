98b6322c580b6ba040ab56a559f6af1a226a3175
guillermo varela <guillovarela16@gmail.com>
2021-04-27 12:28:48 -0300

    Finish post model test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = Post.new(title: 'hello', photo: 'url', content: 'lorem')
+  end
+
+  test 'should be valid if all fields are set' do
+    assert @post.valid?
+  end
+
+  test "should be invalid if title is not present" do
+    @post.title = nil
+
+    assert_not @post.valid?
+  end
+
+  test "should be invalid if photo is not present" do
+    @post.photo = nil
+
+    assert_not @post.valid?
+  end
+
+  test "should be invalid if content is not present" do
+    @post.content = nil
+
+    assert_not @post.valid?
+  end
 end
