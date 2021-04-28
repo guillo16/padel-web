2e06daa6a8972bca3951e5d3de8975b4cd14d2d7
guillermo varela <guillovarela16@gmail.com>
2021-04-28 10:01:46 -0300

    Finish model event test

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 class Event < ApplicationRecord
+  validates :date, presence: true
+  validates :description, presence: true
+  validates :photo, presence: true
+  validates :subtitle, presence: true
+  validates :title, presence: true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @event = Event.new(date: '2021-05-16',
+                       description: 'Un gran torneo',
+                       subtitle: 'El mejor torneo de todos',
+                       photo: 'url',
+                       title: 'Torneo iniciacion'
+                      )
+  end
+
+  test "should be valid if all fields are set" do
+    assert @event.valid?
+  end
+
+  test "should be invalid if date is not set" do
+    @event.date = nil
+
+    assert_not @event.valid?
+  end
+
+  test "should be invalid if description is not set" do
+    @event.description = nil
+
+    assert_not @event.valid?
+  end
+
+  test "should be invalid if subtitle is not set" do
+    @event.subtitle = nil
+
+    assert_not @event.valid?
+  end
+
+  test "should be invalid if photo is not set" do
+    @event.photo = nil
+
+    assert_not @event.valid?
+  end
+
+  test "should be invalid if title is not set" do
+    @event.title = nil
+
+    assert_not @event.valid?
+  end
 end
