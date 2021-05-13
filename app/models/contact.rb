f3c9e9d9191a8d1e17ce8f8dca2b261946459d06
guillermo varela <guillovarela16@gmail.com>
2021-05-13 19:31:56 -0300

    Finish contact page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,20 @@
 class Contact < MailForm::Base
-  attribute :name, validate: true
   attribute :email, validate: /\A[^@\s]+@[^@\s]+\z/i
+  attribute :first_name, validate: true
+  attribute :last_name, validate: true
   attribute :message, validate: true
-  attribute :phone, validate: true
   attribute :nickname, captcha: true
+  attribute :phone, validate: true
 
   def headers
     {
       subject: "Mensaje de clientes de cotton home",
       to: "cottonhome.sn@gmail.com",
-      from: %("#{name}" <#{email}>)
+      from: %("#{self.full_name}" <#{email}>)
     }
+  end
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
   end
 end
